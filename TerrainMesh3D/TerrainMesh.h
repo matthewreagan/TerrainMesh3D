b2d7c78adc9d5b4e303f84565986632e37f8db70
Matt Reagan <matthew.h.reagan@gmail.com>
2016-11-06 14:55:17 -0800

    License headers

--- TerrainMesh3D/TerrainMesh.h
+++ TerrainMesh3D/TerrainMesh.h
@@ -5,6 +5,21 @@
 //  Created by Matt Reagan on 11/6/16.
 //  Copyright © 2016 Matt Reagan. All rights reserved.
 //
+//  This software is provided 'as-is', without any express or implied
+//  warranty. In no event will the author be held liable for any damages
+//  arising from the use of this software.
+//  
+//  Permission is granted to anyone to use this software for any purpose,
+//  including commercial applications, and to alter it and redistribute it
+//  freely, subject to the following restrictions:
+//  
+//  1. The origin of this software must not be misrepresented; you must not
+//     claim that you wrote the original software. If you use this software
+//     in a product, an acknowledgment in the product documentation would be
+//     appreciated but is not required.
+//  2. Altered source versions must be plainly marked as such, and must not be
+//     misrepresented as being the original software.
+//  3. This notice may not be removed or altered from any source distribution.
 
 #import <SceneKit/SceneKit.h>
 
@@ -49,17 +64,25 @@
     intensity, the terrain is raised or lowered.
     
     @param point
-    Relative X,Y location on the terrain mesh
-    (x and y should be in the range of 0.0 - 1.0, where 0,0 is
-    the "bottom left" of the mesh).
+    Relative X,Y location on the terrain mesh. X and Y should
+    be in the range of 0.0 - 1.0, where 0,0 is the lower range
+    vertices, or "bottom left" of the mesh and 1.0,1.0 represents
+    the highest vertices, or "top right" portion of the terrain.
  
     @param brushRadius
-    Size of the deformation */
+    Size of the deformation, this is a relative value, where
+    0.5 is half the size of the mesh. Therefore, a brush at
+    point 0.5, 0.5 (center of terrain) with a radius of 0.5
+    would be a circle that was the same size as the entire
+    terrain. */
 - (void)derformTerrainAt:(CGPoint)point
              brushRadius:(double)brushRadius
                intensity:(double)intensity;
 
+/** Vertices per side that the mesh was initialized with. */
 @property (nonatomic, readonly) int verticesPerSide;
+
+/** Actual length of the terrain, configured on initialization. */
 @property (nonatomic, readonly) double sideLength;
 
 @end
